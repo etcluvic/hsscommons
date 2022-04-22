b9edd2da90268bf8ad682bc20b22491af197c76b
AnshThayil <33091760+AnshThayil@users.noreply.github.com>
2022-04-22 14:44:17 -0700

    changed path location to core

--- components/com_publications/admin/publications.php
+++ components/com_publications/admin/publications.php
@@ -48,7 +48,8 @@ if (!file_exists(__DIR__ . DS . 'controllers' . DS . $controllerName . '.php'))
 	\Route::url('index.php?option=com_publications&controller=batchcreate'),
 	$controllerName == 'batchcreate'
 );
-require_once dirname(dirname(__DIR__)) . DS . 'com_plugins' . DS . 'helpers' . DS . 'plugins.php';
+// require_once dirname(dirname(__DIR__)) . DS . 'com_plugins' . DS . 'helpers' . DS . 'plugins.php';
+require_once(PATH_CORE . DS . 'components' . DS . 'com_plugins' . DS . 'helpers' . DS . 'plugins.php');
 if (\Components\Plugins\Helpers\Plugins::getActions()->get('core.manage'))
 {
 	\Submenu::addEntry(
