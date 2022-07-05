5ac541968107e1ead5eaeb66dad139f09ccc0693
AnshThayil <Ansh.Thayil@yahoo.in>
2022-07-05 10:53:40 -0700

    New template for custom html

---
+++ modules/mod_custom/tmpl/welcome.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * @package    hubzero-cms
+ * @copyright  Copyright (c) 2005-2020 The Regents of the University of California.
+ * @license    http://opensource.org/licenses/MIT MIT
+ */
+
+// no direct access
+defined('_HZEXEC_') or die;
+
+if ($params->get('backgroundimage'))
+{
+	$this->css('
+	.custom' . $moduleclass_sfx . ' {
+		background-image: url(' . $params->get('backgroundimage') . ');
+	}
+	');
+}
+?>
+<div class="custom<?php echo $moduleclass_sfx ?>">
+	<?php echo $module->content; ?>
+    <?php if (!User::isGuest()): ?>
+        <p></p>
+    <?php else: ?>
+        <div class="welcome">
+            <a href="/register" class="btn">Sign Up for a free account today!</a>
+        </div>
+    <?php endif; ?>
+</div>
