39da0e48a104279f0df9b28d5e221cca9e60ba54
Graham Jensen <ghjensen@uvic.ca>
2023-02-09 15:52:07 -0800

    Added new language to account creation page regarding checking spam folder

--- components/com_members/site/views/register/tmpl/update.php
+++ components/com_members/site/views/register/tmpl/update.php
@@ -18,23 +18,23 @@ $this->css('register')
 <section class="main section">
 
 <?php if (isset($this->self) && $this->self) { ?>
-	<p class="passed">Your account has been updated successfully.</p>
+	<p class="passed">Your account has been created or updated successfully.</p>
 	<?php if ($this->updateEmail) { ?>
-		<p>Thank you for updating your account. In order to continue to use this account you must verify your new email address.</p>
+		<p>Thank you for creating or updating your account. In order to continue to use this account you must verify your new email address.</p>
 		<?php if ($this->getError()) { ?>
 			<p class="error"><?php echo $this->getError(); ?></p>
 		<?php } else { ?>
-			<p>A confirmation email has been sent to <?php echo $this->xprofile->get('email'); ?>. You must click the link in that email to activate your account and begin using <?php echo $this->sitename; ?>.</p>
+			<p>A confirmation email has been sent to <?php echo $this->xprofile->get('email'); ?> (if you did not receive this email, please wait a few minutes and also try checking your "Spam" or "Junk" folder). You must click the link in that email to activate your account and begin using <?php echo $this->sitename; ?>.</p>
 		<?php } ?>
 	<?php } ?>
 <?php } else { ?>
-	<p class="passed">The account has been updated successfully.</p>
+	<p class="passed">The account has been created or updated successfully.</p>
 	<?php if ($this->updateEmail) { ?>
-		<p>The user of this account has been notified of the change. In order to continue to use this account they will need to verify the new email address.</p>
+		<p>TThank you for creating or updating your account. In order to continue to use it you will need to verify your new email address.</p>
 		<?php if ($this->getError()) { ?>
 			<p class="error"><?php echo $this->getError(); ?></p>
 		<?php } else { ?>
-			<p>A confirmation email has been sent to <?php echo $this->xprofile->get('email'); ?>. They must click the link in that email to activate your account and begin using <?php echo $this->sitename; ?>.</p>
+			<p>A confirmation email has been sent to <?php echo $this->xprofile->get('email'); ?> (if you did not receive this email, please wait a few minutes and also try checking your "Spam" or "Junk" folder). You must click the link in that email to activate your account and begin using <?php echo $this->sitename; ?>.</p>
 		<?php } ?>
 	<?php } ?>
 <?php } ?>
