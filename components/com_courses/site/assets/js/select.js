3a878ee5b1887ba2465a623640440e221c0b2e94
ghjensen <graham.h.jensen@gmail.com>
2023-02-09 15:09:40 -0800

    Delete components/com_courses directory

--- components/com_courses/site/assets/js/select.js
+++
@@ -1,9 +0,0 @@
-/**
- * @package    hubzero-cms
- * @copyright  Copyright (c) 2005-2020 The Regents of the University of California.
- * @license    http://opensource.org/licenses/MIT MIT
- */
-
-jQuery(function($) {
-	$('.tablesorter').tablesorter();
-});
