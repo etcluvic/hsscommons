c5a26bb00aa4551bab864793d899abb16038fad6
root <root@localhost.localdomain>
2023-05-04 21:36:31 +0000

    Test query

--- plugins/members/repository/repository.php
+++ plugins/members/repository/repository.php
@@ -199,7 +199,7 @@ class plgMembersRepository extends \Hubzero\Plugin\Plugin
 		$pubLog = new \Components\Publications\Tables\Publication($this->_database);
 		$view->pubstats = $pubLog->getRecords(array(
 			"sortby" => "title",		// Default
-			// "status" => array(0,1,3,4,5,6),
+			"status" => array(0,1,3,4,5,6),
 			// 'author' => $uid
 		));
 
