0aa6bed04f73bc8b65bc155928e3fa8805c81ceb
root <root@localhost.localdomain>
2023-05-04 21:40:54 +0000

    Test query

--- plugins/members/repository/repository.php
+++ plugins/members/repository/repository.php
@@ -201,7 +201,7 @@ class plgMembersRepository extends \Hubzero\Plugin\Plugin
 			"sortby" => "title",		// Default
 			"dev" => 1,
 			"status" => array(0,1,3,4,5,6),
-			// 'author' => $uid
+			"author" => $uid
 		));
 
 		// Output HTML
