1e45eeb69f2d6a6a6e4ef04a0ab2d0e81a9cbf5b
Archie To <toanhminh0412@gmail.com>
2023-05-05 12:36:37 +0000

    Test code

--- plugins/projects/publications/publications.php
+++ plugins/projects/publications/publications.php
@@ -1999,7 +1999,8 @@ class plgProjectsPublications extends \Hubzero\Plugin\Plugin
 		if ($confirm && !$agree)
 		{
 			if ($task == 'submit') {
-				App::redirect(Route::url($pub->link('editversion') . '&action=review'));
+				Notify::error(Lang::txt('PLG_PROJECTS_PUBLICATIONS_PUBLICATION_REVIEW_AGREE_TERMS_REQUIRED'), 'projects');
+				App::redirect(Route::url($pub->link('edit') . '&action=review'));
 			} else {
 				// return $pub->project()->member()[0];
 				Notify::error(Lang::txt('PLG_PROJECTS_PUBLICATIONS_PUBLICATION_REVIEW_AGREE_TERMS_REQUIRED'), 'projects');
