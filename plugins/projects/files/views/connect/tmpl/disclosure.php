4c87390c63295cf63bd9fffa145d58f19b7cf783
Archie To <toanhminh0412@gmail.com>
2023-06-29 18:56:07 +0000

    Change url of confirm button

--- plugins/projects/files/views/connect/tmpl/disclosure.php
+++ plugins/projects/files/views/connect/tmpl/disclosure.php
@@ -19,4 +19,4 @@ Log::info($current_url);
     <h3 class="disclosure-title">Google Drive disclosure</h3>
 </div>
 <p class="disclosure-content">HSSCommon’s use and transfer to any other app of information received from Google APIs will adhere to <a href="https://developers.google.com/terms/api-services-user-data-policy#additional_requirements_for_specific_api_scopes" target="_blank">Google API Services User Data Policy</a>, including the Limited Use requirements. To continue, please click the button below.</p>
-<a class="btn btn-primary" href="<?php echo $current_url . '&' . 'disclosure_confirmed=' . $this->projectid; ?>">I agree to give HSSCommons access to my Google Drive</a>
+<a class="btn btn-primary" href="<?php echo $current_url . '&' . 'disclosure_confirmed=1'; ?>">I agree to give HSSCommons access to my Google Drive</a>
