97463a7ba1d6f566d7101885c1d397bf33e1da95
Archie To <toanhminh0412@gmail.com>
2023-07-05 17:27:03 +0000

    1.4.24 - Create an alert when current session is going to time out in 10 minutes

--- templates/mytemplate/js/hub.js
+++ templates/mytemplate/js/hub.js
@@ -287,4 +287,24 @@ jQuery(document).ready(function($){
 			e.preventDefault();
 		}
 	});
+
+	let idleTime = 0;
+	const expiredTime = $('#public-info').data('session-timeout');
+	console.log('Session expired time: ' + expiredTime);
+
+	function countIdleTime() {
+		idleTime++;
+		if (idleTime >= expiredTime - 10) {
+			alert('Your session is about to expired in ' + (expiredTime - idleTime) + ' minutes. Please make sure to save your current work before it getting lost!');
+		}
+	}
+
+	setInterval(countIdleTime, 60000);
+	// Zero the idle timer on mouse movement.
+	$(this).mousemove(function (e) {
+		idleTime = 0;
+	});
+	$(this).keypress(function (e) {
+		idleTime = 0;
+	});
 });
