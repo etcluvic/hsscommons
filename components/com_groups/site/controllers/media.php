c9d4f98a2c93dc707d1c3f565b657106a354b876
Archie To <toanhminh0412@gmail.com>
2023-07-12 21:13:10 +0000

    1.11.13 - Fix a print statements

--- components/com_groups/site/controllers/media.php
+++ components/com_groups/site/controllers/media.php
@@ -648,7 +648,7 @@ class Media extends Base
 		}
 
 		Log::debug($file);
-		Log::debug($_FILES['qqfile']);
+		Log::debug(get_object_vars($_FILES['qqfile']));
 
 		//get folder
 		$folder = Request::getString('folder', '');
