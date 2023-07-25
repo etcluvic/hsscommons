58ede7c84f0cbf6a3b69f690f49ef01288be69cc
Archie To <toanhminh0412@gmail.com>
2023-07-25 20:32:35 +0000

    1.6.4 - Remove print statements

--- modules/mod_collect/helper.php
+++ modules/mod_collect/helper.php
@@ -28,7 +28,6 @@ class Helper extends Module
 	 */
 	public function display()
 	{
-		Log::debug('Calling display()');
 		if (User::isGuest())
 		{
 			return;
@@ -39,16 +38,13 @@ class Helper extends Module
 		$this->model = new Archive('member', User::get('id'));
 
 		$this->item = $this->model->collectible(Request::getCmd('option'));
-		Log::debug('Got item');
 		if (!$this->item->canCollect())
 		{
-			Log::debug('Block 1');
 			return;
 		}
 
 		if (Request::getWord('tryto', '') == 'collect')
 		{
-			Log::debug('Block 2');
 			return $this->collect();
 		}
 
