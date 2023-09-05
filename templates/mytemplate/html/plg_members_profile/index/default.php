f4eba6a9cfd1cc0c25463f89c714d4ca609a7cfb
Archie To <toanhminh0412@gmail.com>
2023-09-05 21:15:48 +0000

    Display ORCID as an url with ORCID logo in front of it on the profile page

--- templates/mytemplate/html/plg_members_profile/index/default.php
+++ templates/mytemplate/html/plg_members_profile/index/default.php
@@ -793,8 +793,17 @@ $profileOrcid = $orcidRow->username;
 				<li class="<?php echo implode(' ', $cls); ?> section" id="input-section-<?php echo $this->escape($field->get('name')); ?>">
 					<div class="section-content">
 						<div class="key"><?php echo $field->get('label'); ?></div>
-						<?php if ($field->get('name') == 'orcid') {?>
-							<div class="value" id="orcid-value" data-orcid="<?php echo $profileOrcid ? $profileOrcid : "" ?>"><a href="<?php echo "https://orcid.org/" . $val; ?>" target="_blank"><?php echo $val; ?></a></div>
+						<?php if ($field->get('name') == 'orcid') {
+							if (!$val) {
+								$val = $profileOrcid ? $profileOrcid : "";
+							}	
+						?>
+							<div class="value" id="orcid-value" data-orcid="<?php echo $profileOrcid ? $profileOrcid : "" ?>">
+								<?php if ($val) { ?>
+								<img src="/core/components/com_members/site/assets/img/orcid_16x16.png" class="logo" width="20" height="20" alt="iD">
+								<?php } ?>
+								<a href="<?php echo "https://orcid.org/" . $val; ?>" target="_blank"><?php echo $val; ?></a>
+							</div>
 						<?php } else { ?>
 							<div class="value"><?php echo (!empty($val)) ? (is_array($val) ? implode(', ', $val) : $val) : Lang::txt('PLG_MEMBERS_PROFILE_NOT_SET'); ?></div>
 						<?php } ?>
