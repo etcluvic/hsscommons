902981ab3dae525126b0cd871e0db538df290ad0
Archie To <toanhminh0412@gmail.com>
2023-10-17 17:00:54 +0000

    1.15.33 - Update French language for support page

--- templates/mytemplate/html/com_support/index/display.php
+++ templates/mytemplate/html/com_support/index/display.php
@@ -43,23 +43,23 @@ $this->css('index.css');
 
 <section class="section">
 	<div class="tagline">
-		<p>We offer several ways of finding content and encourage exploring our knowledge base and engaging the community for support.</p>
+		<p><?php echo Lang::txt('COM_SUPPORT_TAGLINE'); ?></p>
 	</div>
 
 	<?php if (Component::isEnabled('com_kb')) { ?>
 	<div class="about odd kb">
-		<h3>Knowledge Base</h3>
-		<p><a href="<?php echo Route::url('index.php?option=com_kb'); ?>">Find</a> answers to frequently asked questions, helpful tips, and any other information we thought might be useful.</p>
+		<h3><?php echo Lang::txt('COM_SUPPORT_KNOWLEDGE_BASE'); ?></h3>
+		<p><a href="<?php echo Route::url('index.php?option=com_kb'); ?>"><?php echo Lang::txt('COM_SUPPORT_FIND'); ?></a> <?php echo Lang::txt('COM_SUPPORT_KNOWLEDGE_BASE_TAGLINE'); ?></p>
 	</div>
 	<?php } ?>
 
 	<div class="about even report">
-		<h3>Report problems</h3>
-		<p><a href="<?php echo Route::url('index.php?option=com_support&task=new'); ?>">Report problems</a> with our form and have your problem entered into our <a href="<?php echo Route::url('index.php?option=com_support&task=tickets'); ?>">ticket tracking system</a>. We guarantee a response!</p>
+		<h3><?php echo Lang::txt('COM_SUPPORT_REPORT_PROBLEMS'); ?></h3>
+		<p><a href="<?php echo Route::url('index.php?option=com_support&task=new'); ?>"><?php echo Lang::txt('COM_SUPPORT_REPORT_PROBLEMS'); ?></a> <?php echo Lang::txt('COM_SUPPORT_REPORT_PROBLEMS_TAGLINE'); ?> <a href="<?php echo Route::url('index.php?option=com_support&task=tickets'); ?>"><?php echo Lang::txt('COM_SUPPORT_TICKET_TRACKING_SYSTEM'); ?></a>. <?php echo Lang::txt('COM_SUPPORT_REPORT_PROBLEMS_GUARANTEE_RESPONSE'); ?></p>
 	</div>
 
 	<div class="about odd tickets">
-		<h3>Track Tickets</h3>
-		<p>Have a problem entered into our <a href="<?php echo Route::url('index.php?option=com_support&task=tickets'); ?>">ticket tracking system</a>? Track its progress, add comments and notes, or close resolved issues.</p>
+		<h3><?php echo Lang::txt('COM_SUPPORT_TRACK_TICKETS'); ?></h3>
+		<p><?php echo Lang::txt('COM_SUPPORT_TRACK_TICKETS_PART_ONE'); ?> <a href="<?php echo Route::url('index.php?option=com_support&task=tickets'); ?>"><?php echo Lang::txt('COM_SUPPORT_TICKET_TRACKING_SYSTEM'); ?></a>? <?php echo Lang::txt('COM_SUPPORT_TRACK_TICKETS_PART_TWO'); ?></p>
 	</div>
 </section>
