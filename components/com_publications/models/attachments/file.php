bd1c81dfa0d6ea9e39bc9bad082c5901382d0b0f
Archie To <toanhminh0412@gmail.com>
2023-10-27 16:48:08 +0000

    Publication file preview - Try not using the accept range header

--- components/com_publications/models/attachments/file.php
+++ components/com_publications/models/attachments/file.php
@@ -795,7 +795,7 @@ class File extends Base
 				$server->acceptranges(true);
 				$server->saveas(basename($download));
 
-				if ($serveInline && !$server->serve_inline($download, false))
+				if ($serveInline && !$server->serve_inline($download))
 				{
 					// Should only get here on error
 					throw new Exception(Lang::txt('PLG_PROJECTS_PUBLICATIONS_ERROR_SERVE'), 404);
