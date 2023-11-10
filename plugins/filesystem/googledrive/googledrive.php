d31b12a3eafe206aac343614466cea338f64353f
Archie To <toanhminh0412@gmail.com>
2023-11-10 19:30:52 +0000

    Google Drive verification - Test using drive app readonly scope

--- plugins/filesystem/googledrive/googledrive.php
+++ plugins/filesystem/googledrive/googledrive.php
@@ -36,7 +36,7 @@ class plgFilesystemGoogleDrive extends \Hubzero\Plugin\Plugin
 		$client->setClientId($app_id);
 		$client->setClientSecret($app_secret);
 		$client->addScope(Google_Service_Drive::DRIVE_FILE);
-		$client->addScope('https://www.googleapis.com/auth/auth/drive.apps.readonly');
+		$client->addScope('https://www.googleapis.com/auth/drive.apps.readonly');
 		$client->setAccessType('offline');
 		$client->setApprovalPrompt('force');
 		$client->setIncludeGrantedScopes(true);
