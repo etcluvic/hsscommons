8588f02542d0a5bc10e64502fc7ab3e81c261a64
Archie To <toanhminh0412@gmail.com>
2023-11-10 19:12:37 +0000

    Google Drive verification - Test adding 2 other scopes

--- plugins/filesystem/googledrive/googledrive.php
+++ plugins/filesystem/googledrive/googledrive.php
@@ -37,6 +37,8 @@ class plgFilesystemGoogleDrive extends \Hubzero\Plugin\Plugin
 		$client->setClientSecret($app_secret);
 		$client->addScope(Google_Service_Drive::DRIVE_FILE);
 		$client->addScope('https://www.googleapis.com/auth/drive.install');
+		$client->addScope('https://www.googleapis.com/auth/drive.appdata');
+		$client->addScope('https://www.googleapis.com/auth/drive.appfolder');
 		$client->setAccessType('offline');
 		$client->setApprovalPrompt('force');
 		$client->setIncludeGrantedScopes(true);
