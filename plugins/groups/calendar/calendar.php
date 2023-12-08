a3c1c66a8db02edecd4a8dc140975e566a2e639c
Archie To <toanhminh0412@gmail.com>
2023-12-08 19:02:44 +0000

    Group event registration - Test code

--- plugins/groups/calendar/calendar.php
+++ plugins/groups/calendar/calendar.php
@@ -2078,8 +2078,8 @@ class plgGroupsCalendar extends \Hubzero\Plugin\Plugin
 		}
 
 		// User must be a member of this group
-		Log::debug('Group id: '. $this->group->get('id'));
-		Log::debug('User id: ' . User::get('id'));
+		Log::debug($this->group->get('id'));
+		Log::debug(User::get('id'));
 
 		$target_dir = PATH_APP . DS . 'site' . DS . 'events' . DS . $event_id . DS . 'respondents' . DS . $respondent_id . DS . 'uploads';
 		if (is_dir($target_dir) && $files = Filesystem::files($target_dir))
@@ -2091,6 +2091,7 @@ class plgGroupsCalendar extends \Hubzero\Plugin\Plugin
 			$server->serve();
 		} else {
 			$this->setError(Lang::txt('File not found'));
+			return false;
 		}
 
 		return $this->details();
