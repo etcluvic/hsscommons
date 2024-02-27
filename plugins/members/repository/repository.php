786bbca212ee62e29d71df132d3732d4c8d88a7f
Minh To <toanhminh0412@gmail.com>
2024-02-26 21:01:24 -0800

    Issue #169: Change print statements fo debug

--- plugins/members/repository/repository.php
+++ plugins/members/repository/repository.php
@@ -236,7 +236,12 @@ class plgMembersRepository extends \Hubzero\Plugin\Plugin
 			$pubFound = false;
 			foreach ($authorPubstats as $authorPub) {
 				Log::debug(get_object_vars($pub));
-				Log::debug(get_object_vars($authorPub));
+				if gettype($authorPub) == "object" {
+					Log::debug(get_object_vars($authorPub));
+				} else {
+					Log::debug($authorPub);
+				}
+				
 				if ($pub->id == $authorPub->id) {
 					$pub = true;
 					break;
