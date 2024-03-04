fce1e8459998e87064a64475ce1b1467f7bed587
Archie To <toanhminh0412@gmail.com>
2024-03-03 19:09:59 -0800

    Issue #169: Publication not found - Add a print statement to debug

--- components/com_publications/site/views/about/tmpl/default.php
+++ components/com_publications/site/views/about/tmpl/default.php
@@ -7,7 +7,7 @@
 
 // No direct access
 defined('_HZEXEC_') or die();
-
+Log::debug('Call this template');
 $webpath = $this->config->get('webpath');
 
 $authorized = $this->publication->access('view-all');
