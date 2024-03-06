5285de9df85e5e53ad04ee3dcd8fcc402ae79189
Archie To <toanhminh0412@gmail.com>
2024-03-05 18:55:12 -0800

    Issue #176: Attempt to add members to project from group once removed

--- plugins/projects/team/team.php
+++ plugins/projects/team/team.php
@@ -792,6 +792,8 @@ class plgProjectsTeam extends \Hubzero\Plugin\Plugin
 					
 					// When edit team. Once hit "Sync member as", sync all group members to that role, except for the project creator
 					} else {
+						$objO = $this->model->table('Owner');
+						$objO->saveOwners($this->model->get('id'), User::get('id'), 0, $this->model->get('owned_by_group'), $syncRole, 1, 1, '', $split_group_roles = 0);
 						// Get all the members that are in this project and in the owner group
 						$groupOwnerMembers = $this->model->groupOwner()->get('members');
 
@@ -801,7 +803,6 @@ class plgProjectsTeam extends \Hubzero\Plugin\Plugin
 							->from('#__projects')  
 							->whereEquals('id', $this->model->get('id'))  
 							->fetch();
-						Log::debug($users);
 
 						$syncedMembers = array();
 						if (count($users) > 0) {
