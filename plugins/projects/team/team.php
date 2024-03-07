386e3590b2134e76a366b8fca7ecdefd869c76f7
Archie To <toanhminh0412@gmail.com>
2024-03-06 19:46:01 -0800

    Issue #176: Remove the previous print statement

--- plugins/projects/team/team.php
+++ plugins/projects/team/team.php
@@ -126,8 +126,6 @@ class plgProjectsTeam extends \Hubzero\Plugin\Plugin
 			$this->_uid      = User::get('id');
 			$this->_config   = $model->config();
 
-			Log::debug($this->_task);
-
 			switch ($this->_task)
 			{
 				case 'edit':
