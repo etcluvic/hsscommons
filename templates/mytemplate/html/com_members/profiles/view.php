dd277bb418f876c4c2ec218735a1df960c621136
root <root@dma1bcpbi32.bb.uvic.ca>
2024-10-03 09:30:41 +0000

    Re-route member/contributions url

--- templates/mytemplate/html/com_members/profiles/view.php
+++ templates/mytemplate/html/com_members/profiles/view.php
@@ -172,6 +172,10 @@ $profileOrcid = $orcidRow->username;
 					}
 					$name = $c[$key];
 					$url = Route::url($this->profile->link() . '&active=' . $key);
+					if ($key == 'contributions') 
+					{
+						$url = Route::url($this->profile->link() . '&active=' . $key . '&area=impact');
+					}
 					$cls = ($this->active == $key) ? 'active' : '';
 					$tab_name = ($this->active == $key) ? $name : $tab_name;
 
