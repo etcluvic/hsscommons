b4f730c153b87213a01e1e60ae8acc66e594387b
Archie To <toanhminh0412@gmail.com>
2024-10-24 01:10:48 +0000

    Add updates from 2.4-mains

--- plugins/publications/googlescholar/googlescholar.php
+++ plugins/publications/googlescholar/googlescholar.php
@@ -90,7 +90,7 @@ class plgPublicationsGooglescholar extends \Hubzero\Plugin\Plugin
 
 		foreach ($publication->_authors as $contributor)
 		{
-			if (strtolower($contributor->role) == 'submitter')
+			if ($contributor->role && strtolower($contributor->role) == 'submitter')
 			{
 				continue;
 			}
