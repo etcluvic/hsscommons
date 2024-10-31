1c416130ca2688ea97052a1778cc2257716c08cc
Archie To <toanhminh0412@gmail.com>
2024-10-31 00:26:42 +0000

    Fix deprecated code
    Avoid passing null as the second param to preg_match_all

--- plugins/projects/publications/views/draft/tmpl/license.php
+++ plugins/projects/publications/views/draft/tmpl/license.php
@@ -105,8 +105,8 @@ echo $complete == 1 ? ' el-complete' : ' el-incomplete'; echo ($complete == 0 &&
 						</div>
 					<?php } else {
 						// Word replacements required?
-						preg_match_all('/\[([^\]]*)\]/', $this->license->text, $substitutes);
-						preg_match_all('/\[([^]]+)\]/', $this->pub->license_text, $matches);
+						preg_match_all('/\[([^\]]*)\]/', $this->license->text ? $this->license->text : "", $substitutes);
+						preg_match_all('/\[([^]]+)\]/', $this->pub->license_text ? $this->pub->license_text : "", $matches);
 						$i = 0;
 
 						if ($this->license->text && isset($substitutes[1]) && !empty($substitutes[1])) { ?>
