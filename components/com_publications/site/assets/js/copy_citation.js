8356dc122408fc7eeea0fd96f158f3e087f1e504
lee <leenapthine@uvic.ca>
2024-12-13 21:49:12 +0000

    Add logic to new json and csv buttons

--- components/com_publications/site/assets/js/copy_citation.js
+++ components/com_publications/site/assets/js/copy_citation.js
@@ -26,3 +26,99 @@ jQuery(document).ready(function ($) {
             });
     });
 });
+
+//-----------------------------
+// Export citation to JSON
+//-----------------------------
+
+// Function to trigger download of JSON file
+function downloadJSONFile(jsonString, filename) {
+    const blob = new Blob([jsonString], { type: 'application/json' });
+    const url = URL.createObjectURL(blob);
+    const a = document.createElement('a');
+    a.href = url;
+    a.download = filename;
+    document.body.appendChild(a);
+    a.click();
+    document.body.removeChild(a);
+    URL.revokeObjectURL(url);
+}
+
+jQuery(document).ready(function ($) {
+    $('.export-jsoncitation').on('click', function () {
+
+        // Get the JSON citation
+        const citation_json = getMeta('json');
+
+        // Download JSON file
+        downloadJSONFile(citation_json, 'citation.json');
+    });
+});
+
+//-----------------------------
+// Export citation to CSV
+//-----------------------------
+
+// Function to trigger download of CSV file
+function downloadCSVFile(csvString, filename) {
+    const blob = new Blob([csvString], { type: 'text/csv' });
+    const url = URL.createObjectURL(blob);
+    const a = document.createElement('a');
+    a.href = url;
+    a.download = filename;
+    document.body.appendChild(a);
+    a.click();
+    document.body.removeChild(a);
+    URL.revokeObjectURL(url);
+}
+
+
+jQuery(document).ready(function ($) {
+    $('.export-csvcitation').on('click', function () {
+
+        // Get the CSV citation
+        const citation_csv = getMeta('csv');
+
+        // Download CSV file
+        downloadCSVFile(citation_csv, 'citation.csv');
+    });
+});
+
+//-----------------------------
+// Parse Metadata
+//-----------------------------
+
+function getMeta(format) {
+    const metas = document.getElementsByTagName('meta');
+    var citation = {};
+
+    for (let i = 0; i < metas.length; i++) {
+        const name = metas[i].getAttribute('name');
+        if (name && name.substring(0, 9) === 'citation_') {
+            const key = name.substring(9);
+            var content = metas[i].getAttribute('content');
+            content = content.replace(/[‘’]/g, "'"); // Wrong type of apostraphe in metadata
+            citation[key] = content;
+        }
+    }
+    if (format === 'json') {
+        return JSON.stringify(citation, null, 2);
+    } else if (format === 'csv') {
+        return convertToCSV(citation);
+    }
+
+    return "";
+}
+
+function convertToCSV(obj) {
+    const keys = Object.keys(obj);
+    const values = Object.values(obj).map(value => {
+        if (typeof value === 'string' && value.includes(',')) {
+            value = value.replace(/"/g, '""'); // Escape double quotes
+            return `"${value}"`; // Wrap value in double quotes
+        }
+        return value;
+    });
+    const csv = keys.join(',') + '\n' + values.join(',');
+    return csv;
+}
