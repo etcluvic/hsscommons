4298ca1fd257ad5b4a0e075ee74e597b84429ae7
Archie To <toanhminh0412@gmail.com>
2025-10-08 20:00:00 +0000

    Fix deprecated code
    Avoid passing null to strip_tags

--- components/com_search/models/basic/result/assocscalar.php
+++ components/com_search/models/basic/result/assocscalar.php
@@ -89,7 +89,7 @@ class AssocScalar extends SearchResult
 		self::assert_keys(array('title', 'description', 'link'), $row);
 		foreach ($row as $key => $val)
 		{
-			$this->$key = is_array($val) ? array_map('stripslashes', array_map('strip_tags', $val)) : stripslashes(strip_tags($val));
+			$this->$key = is_array($val) ? array_map('stripslashes', array_map('strip_tags', $val)) : stripslashes(strip_tags($val ?? ''));
 		}
 
 		if ($this->weight === null)
